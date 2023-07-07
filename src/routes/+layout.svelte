bb0050b2f3d5c340e281a2ccf7c809501e96f0ff
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-07 23:11:03 +0700

    redo the page, first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,112 +1,49 @@
-<script>
-	import '../app.css';
-	import Header from '../components/Header.svelte';
-	import Logo from '../components/Logo.svelte';
-	import Window from '../components/Window.svelte';
-	import Work from '../components/Work.svelte';
-	import Table from '../components/Table.svelte';
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<svelte:head>
-	<title>okreate</title>
-	<link
-		rel="stylesheet"
-		href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-	/>
-	<script src="https://kit.fontawesome.com/ecb271c71c.js" crossorigin="anonymous"></script>
-</svelte:head>
-
-<Header />
-
-<Window>
-	<span id="about" slot="name" class="scroll-mt-main">about</span>
-	<div slot="top">
-		<div class="logo mx-auto mt-3 mb-8 w-60">
-			<Logo />
-		</div>
-	</div>
-	<p slot="inner" class="text-white lg:text-4xl text-3xl text-center font-bold">
-		<span class="text-accent font-mono">Hello world</span>, I'm Muhamad Oka Augusta. I'm a web
-		developer, instructional designer, video editor, and student based in Bandung, Indonesia.
-	</p>
-</Window>
-
-<Work />
-
-<Window>
-	<span id="other-work" slot="name">other work</span>
-	<Table />
-</Window>
-
-<Window>
-	<span id="social" slot="name">social</span>
-	<div class="flex justify-around">
-		<div class="social px-5 py-5 lg:px-16 lg:py-10">
-			<i class="fa-brands text-5xl lg:text-8xl fa-instagram" />
-			<a class="btn capitalize" href="https://www.instagram.com/oka.augusta" target="_blank"
-				>instagram</a
-			>
-		</div>
-		<div class="social px-5 py-5 lg:px-16 lg:py-10">
-			<i class="fa-brands text-5xl lg:text-8xl fa-behance" />
-			<a class="btn capitalize" href="https://www.behance.net/" target="_blank">Behance</a>
-		</div>
-		<div class="social px-5 py-5 lg:px-16 lg:py-10">
-			<i class="fa-brands text-5xl lg:text-8xl fa-linkedin-in" />
-			<a class="btn capitalize" href="https://www.linkedin.com/in/okaaugust/" target="_blank"
-				>Linkedin</a
-			>
-		</div>
-	</div>
-</Window>
-
-<Window>
-	<span id="contact" slot="name" class="scroll-mt-main">contact</span>
-	<div slot="inner">
-		<p class="text-white lg:text-4xl text-3xl text-center font-bold">
-			Interested in working with me? or just want a little chat? Get&nbsp;in&nbsp;touch!
-		</p>
-		<a href="mailto:okreate@gmail.com" class="btn mx-auto mt-5 text-3xl"> Contact Me </a>
-	</div>
-</Window>
-
-<style global lang="postcss">
-	body {
-		background-color: var(--root-bg);
-	}
-
-	.btn {
-		display: block;
-		width: fit-content;
-		padding: 6px 18px;
-		background-color: var(--accent);
-		border-radius: 1.5rem;
-		color: var(--white);
-		font-weight: bold;
-		border: 2px solid transparent;
-
-		transition: all 500ms;
-
-		&:hover {
-			border: 2px solid var(--white);
-		}
-
-		&:active {
-			color: var(--root-bg);
-		}
-	}
-
-	.social {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		/* padding: 2.5rem 4rem; */
-		border: 1px solid var(--inner-container-accent);
-		background-color: var(--inner-container-bg);
-	}
-
-	.fa-brands {
-		color: var(--white);
-		margin-bottom: 34px;
-	}
-</style>
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,14 @@
+import { join } from 'path'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			accent: '#2d8cea',
-			containerbg: '#232323',
-			innercontainerbg: '#1D1D1D',
-			innercontaineraccent: '#2A2A2A',
-			rootbg: '#161616',
-			white: '#ededed',
-			gray: '#b1b1b1',
-			green: '#22d699',
-			darkgreen: '#176a4c',
-			purple: '#a690e2',
-			orange: '#eea53c'
-		},
-		extend: {
-			scrollMargin: {
-				main: '4.35rem'
-			}
-		}
+		extend: {},
 	},
-	plugins: []
-};
+	plugins: [forms,typography,...skeleton()],
+}
