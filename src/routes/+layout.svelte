f27c1672d7e09f4ff6cd0ba8617c3b6f04059aee
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-08 00:07:22 +0700

    add custom theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
-<script lang='ts'>
+<script lang="ts">
+	// Your custom Skeleton theme:
+	import '../theme.postcss';
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
