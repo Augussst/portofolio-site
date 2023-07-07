8d346c8e6910bf19037b7fdd1672db33b7157516
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-08 01:48:14 +0700

    get decap cms work with mdsvex

---
+++ src/routes/posts/+page.js
@@ -0,0 +1,15 @@
+// const modules = Object.entries(import.meta.glob('../../posts/blog/*.md'));
+// console.log(modules);
+
+// const getRoutes = () => {
+// 	return modules.map(([file, module]) => {
+// 		const path = file.replace('./routes/', '/').replace('index', '').replace('.md', '');
+
+// 		return {
+// 			path,
+// 			...module.metadata
+// 		};
+// 	});
+// };
+
+// export { getRoutes };
---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { getRoutes } from '../../metadata';
+
+	const routes = getRoutes();
+	console.log(routes);
+</script>
+
+<ul>
+	{#each routes as route}
+		<li>
+			<a href={route.path}>{route.title}</a>
+		</li>
+	{/each}
+</ul>
