7070219a591ec30e5c780b52b2c03d77626887ab
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-10 00:48:14 +0700

    import embed content from cms

---
+++ src/routes/+page.js
@@ -0,0 +1,26 @@
+import { error } from '@sveltejs/kit';
+
+export async function load() {
+	try {
+		let embeds = [];
+		const paths = await import.meta.glob('$content/embeds/*.md', { eager: true });
+
+		for (const path in paths) {
+			const file = paths[path];
+			const slug = path.split('/').at(-1)?.replace('.md', '');
+			const content = file.default;
+
+			if (file && typeof file === 'object' && 'metadata' in file && slug) {
+				const metadata = file.metadata;
+				const post = { slug, ...metadata, content };
+				embeds.push(post);
+			}
+		}
+
+		return {
+			embeds
+		};
+	} catch (e) {
+		throw error(404, `Could not show post`);
+	}
+}
