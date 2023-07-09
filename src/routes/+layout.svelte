bd2ae540f10599093672a2d90ddb619fc97ef7da
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-10 00:47:36 +0700

    add header and font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,49 +5,26 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	// Fonts
+	import '@fontsource/staatliches';
+	import '@fontsource/ubuntu';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Header from '../lib/components/Header.svelte';
 
-	// Site setting
 	import * as sites from '$content/settings.md';
 	const site = sites.metadata;
 </script>
 
+<svelte:head>
+	<title>{site.siteName}</title>
+</svelte:head>
+
 <!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">{site.siteName}</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
+
+<AppShell slotPageContent="container mx-auto">
+	<svelte:fragment slot="pageHeader"><Header /></svelte:fragment>
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
 </AppShell>
