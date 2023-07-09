c1336e7d1f3e19d5a8c6102086895b09f00b18ed
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-10 00:50:22 +0700

    add hero for front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<script>
-	import { LightSwitch } from '@skeletonlabs/skeleton';
-</script>
-
-<LightSwitch />
+<section class="relative flex flex-col py-5 px-2 text-center gap-5">
+	<h1 class="h1">
+		Hello,<br /> I'm a
+	</h1>
+	<div class="grid grid-cols-3">
+		<div class="flex items-center justify-center text-center h1">Web developer</div>
+		<div class="flex items-center justify-center text-center h1">&</div>
+		<div class="flex items-center justify-center text-center h1">Instructional Designer</div>
+	</div>
+	<h1 class="h1">based in indonesia</h1>
+	<!-- <img
+		class="absolute top-0 -z-10 h-full left-0 right-0 mx-auto blur-[2px]"
+		src="/images/uploads/me.png"
+		alt="my pic"
+	/> -->
+</section>
