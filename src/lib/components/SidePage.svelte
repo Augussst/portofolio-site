82ee05dffd95a9edb6739dfb63088f763b338e11
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-22 21:18:35 +0700

    rename content comp to side page

---
+++ src/lib/components/SidePage.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let transition;
+	export let trigger;
+	export let margin;
+</script>
+
+<div
+	class="{trigger ? `lg:w-5/6 max-lg:h-5/6 ${margin} visible` : 'w-0 h-0 invisible'}
+                border rounded-xl {transition}
+    "
+>
+	<slot />
+</div>
