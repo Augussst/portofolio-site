3c6874f8109ee32cae80933bf70186d05e993190
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-22 21:20:35 +0700

    move page.js to projects path

--- src/routes/[projects]/+page.js
+++ src/routes/[projects]/+page.js
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 export async function load() {
 	try {
 		let embeds = [];
-		const paths = await import.meta.glob('$content/embeds/*.md', { eager: true });
+		const paths = await import.meta.glob('$cms/embeds/*.md', { eager: true });
 
 		for (const path in paths) {
 			const file = paths[path];
