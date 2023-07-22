a3c8b2c274e9ebc418fb68d3dfb3078da599252c
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-22 22:02:40 +0700

    add project page

---
+++ src/routes/[projects]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	const data = $page.data;
+</script>
+
+{#key data.url}
+	<section class="p-3" in:fade={{ delay: 250 }} out:fade>
+		<h1>This is left</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi optio quasi obcaecati
+			dignissimos, distinctio expedita beatae aliquam nemo quis vero, repellendus, sapiente enim
+			rem? Rerum doloremque quibusdam molestias veniam.
+		</p>
+	</section>
+{/key}
