93da79fecfad444b92a012e747696c7f8db094ef
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-23 23:38:14 +0700

    disable blog import

--- src/routes/posts/+page.js
+++ src/routes/posts/+page.js
@@ -1,14 +1,14 @@
-import { error } from '@sveltejs/kit';
+// import { error } from '@sveltejs/kit';
 
-export async function load() {
-	try {
-		const post = await import('$content/blog/2023-07-08-test1.md');
+// export async function load() {
+// 	try {
+// 		const post = await import('$content/blog/2023-07-08-test1.md');
 
-		return {
-			content: post.default,
-			meta: post.metadata
-		};
-	} catch (e) {
-		throw error(404, `Could not show post`);
-	}
-}
+// 		return {
+// 			content: post.default,
+// 			meta: post.metadata
+// 		};
+// 	} catch (e) {
+// 		throw error(404, `Could not show post`);
+// 	}
+// }
