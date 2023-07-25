a09cb2cf77af233fe50be50333026adb14374298
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-25 23:10:51 +0700

    add project type filter page

---
+++ src/routes/projects/[projectType]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+</script>
+
+<section class="p-3">
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+	<h1>This is left</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi optio quasi obcaecati
+		dignissimos, distinctio expedita beatae aliquam nemo quis vero, repellendus, sapiente enim rem?
+		Rerum doloremque quibusdam molestias veniam.
+	</p>
+</section>
