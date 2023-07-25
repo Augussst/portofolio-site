4ed2dcddfcdb490e16b4a5f6789b1a1d1de4b4f9
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-25 23:09:15 +0700

    add project data import

---
+++ src/routes/projects/+layout.js
@@ -0,0 +1,42 @@
+/** @type {import('@sveltejs/kit').Load} */
+import { error } from '@sveltejs/kit';
+
+export const load = async ({ params }) => {
+	try {
+		const projectTypeMetadata = await import('$cms/settings/project.md');
+		const projectTypes = projectTypeMetadata.metadata.projectTypes;
+		let projects = [];
+
+		let paths = await import.meta.glob(`$cms/projects/**`, { eager: true });
+
+		for (const path in paths) {
+			const file = paths[path];
+			const slug = path.split('/').at(-1)?.replace('.md', '');
+			const content = file.default;
+
+			if (file && typeof file === 'object' && 'metadata' in file && slug) {
+				const metadata = file.metadata;
+				const project = { slug, ...metadata, content };
+				projects.push(project);
+			}
+		}
+
+		const projectByTypes = () => {
+			const projectByTypes = projectTypes.map((type) => {
+				const typeProjects = projects.filter((project) => project.projectType === type.slug);
+				const projectsWithoutType = typeProjects.map(({ ...rest }) => rest); // Destructuring to remove projectType property
+				return { ...type, projects: projectsWithoutType };
+			});
+
+			return projectByTypes;
+		};
+
+		return {
+			params,
+			projectTypes,
+			projectByTypes: projectByTypes()
+		};
+	} catch (e) {
+		throw error(404, `Could not show projects`);
+	}
+};
