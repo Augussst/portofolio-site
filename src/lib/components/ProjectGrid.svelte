20e37ae9a3eb52aa03bf0d754929a6a4a21fa377
Muhamad Oka Augusta <oka.august@gmail.com>
2023-07-27 23:32:17 +0700

    add project components

---
+++ src/lib/components/ProjectGrid.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let typeIcon;
+	export let typeTitle;
+	let className;
+	export { className as class };
+</script>
+
+<h1 class="h1 flex items-center gap-2 text-2xl">
+	<iconify-icon icon={typeIcon} />
+	{typeTitle}
+</h1>
+<div class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pt-3 {className}">
+	<slot />
+</div>
---
+++ src/lib/components/ProjectItem.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let href;
+	export let projectThumbnail;
+	export let thumbnailFallback;
+	export let projectTitle;
+</script>
+
+<a
+	class="flex flex-col btn items-center gap-2 border rounded border-primary-500 p-1"
+	href="./projects/{href}"
+>
+	<div class="relative aspect-video w-full h-auto">
+		<div
+			style="
+						background: linear-gradient(
+							rgba(0, 0, 0, 0.7),
+							rgba(0, 0, 0, 0.7)
+						),
+						url({projectThumbnail !== undefined ? projectThumbnail : thumbnailFallback});
+						background: no-repeat;
+						background-position: center;
+						"
+			class="absolute w-full h-full"
+		/>
+		<img
+			class="relative w-auto z-10 h-full mx-auto"
+			src={projectThumbnail !== undefined ? projectThumbnail : thumbnailFallback}
+			alt="{projectTitle} image"
+		/>
+	</div>
+	<h2 class="h2 !ml-0 text-lg text-center whitespace-normal font-token">
+		{projectTitle}
+	</h2>
+</a>
