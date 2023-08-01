7e0b72106f47c4521a41e84e31d1f3807c6cd2e6
Muhamad Oka Augusta <oka.august@gmail.com>
2023-08-01 23:14:15 +0700

    add animation and add scroll to top on item click

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,25 +1,38 @@
 <script>
 	import ProjectGrid from '$lib/components/ProjectGrid.svelte';
 	import ProjectItem from '$lib/components/ProjectItem.svelte';
+	import { fly } from 'svelte/transition';
+	import { section } from './+layout.svelte';
 
 	export let data;
 	$: ({ projectByTypes, site } = data);
 	let lastItem = (a, i) => i == a.length - 1;
+
+	function topFunction() {
+		section.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 </script>
 
-{#each projectByTypes as type, i}
-	<ProjectGrid
-		typeTitle={type.title}
-		typeIcon={type.icon}
-		class={lastItem(projectByTypes, i) ? '' : 'mb-3'}
-	>
-		{#each type.projects as project}
-			<ProjectItem
-				projectTitle={project.title}
-				href="{type.slug}/{project.slug}"
-				projectThumbnail={project.thumbnail}
-				thumbnailFallback={site.siteLogo}
-			/>
+{#key data.url}
+	<div in:fly={{ x: -100, duration: 500, delay: 500 }} out:fly={{ x: -100, duration: 500 }}>
+		{#each projectByTypes as type, i}
+			<ProjectGrid
+				typeTitle={type.title}
+				typeIcon={type.icon}
+				class={lastItem(projectByTypes, i) ? '' : 'mb-3'}
+			>
+				{#each type.projects as project}
+					<ProjectItem
+						click={() => {
+							topFunction();
+						}}
+						projectTitle={project.title}
+						href="{type.slug}/{project.slug}"
+						projectThumbnail={project.thumbnail}
+						thumbnailFallback={site.siteLogo}
+					/>
+				{/each}
+			</ProjectGrid>
 		{/each}
-	</ProjectGrid>
-{/each}
+	</div>
+{/key}
--- src/routes/projects/[projectType]/+page.svelte
+++ src/routes/projects/[projectType]/+page.svelte
@@ -1,22 +1,35 @@
 <script>
 	import ProjectGrid from '$lib/components/ProjectGrid.svelte';
 	import ProjectItem from '$lib/components/ProjectItem.svelte';
+	import { fly } from 'svelte/transition';
+	import { section } from '../+layout.svelte';
 
 	export let data;
 	$: ({ projectByTypes, params, site } = data);
+
+	function topFunction() {
+		section.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 </script>
 
-{#each projectByTypes as type}
-	{#if type.slug == params.projectType}
-		<ProjectGrid typeTitle={type.title} typeIcon={type.icon}>
-			{#each type.projects as project}
-				<ProjectItem
-					projectTitle={project.title}
-					href="{type.slug}/{project.slug}"
-					projectThumbnail={project.thumbnail}
-					thumbnailFallback={site.siteLogo}
-				/>
-			{/each}
-		</ProjectGrid>
-	{/if}
-{/each}
+{#key data.url}
+	<div in:fly={{ x: -100, duration: 500, delay: 500 }} out:fly={{ x: -100, duration: 500 }}>
+		{#each projectByTypes as type}
+			{#if type.slug == params.projectType}
+				<ProjectGrid typeTitle={type.title} typeIcon={type.icon}>
+					{#each type.projects as project}
+						<ProjectItem
+							click={() => {
+								topFunction();
+							}}
+							projectTitle={project.title}
+							href="{type.slug}/{project.slug}"
+							projectThumbnail={project.thumbnail}
+							thumbnailFallback={site.siteLogo}
+						/>
+					{/each}
+				</ProjectGrid>
+			{/if}
+		{/each}
+	</div>
+{/key}
