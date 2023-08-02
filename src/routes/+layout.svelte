18887bac0c614d68b5e6570a7edf4025475bd672
Muhamad Oka Augusta <oka.august@gmail.com>
2023-08-02 22:11:28 +0700

    add popup for small screen filter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,9 @@
 	import '@fontsource/ubuntu';
 
 	import PageSection from '$lib/components/PageSection.svelte';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
 	export let data;
 	$: ({ site } = data);
--- src/routes/projects/+layout.svelte
+++ src/routes/projects/+layout.svelte
@@ -7,39 +7,81 @@
 </script>
 
 <script>
+	import { popup } from '@skeletonlabs/skeleton';
 	export let data;
 	$: ({ params, projectTypes } = data);
 	$: activeClass = (slug) => {
 		return params.projectType === slug ? 'bg-primary-500 text-secondary-500' : '';
 	};
+
+	const projectTypePopup = {
+		event: 'click',
+		target: 'projectTypePopup',
+		placement: 'bottom-start'
+	};
+
+	$: selectedType = () => {
+		const selected = projectTypes.filter((type) => type.slug === params.projectType);
+		return selected[0]?.title;
+	};
 </script>
 
 {#if params?.project == undefined}
-	<ul class="flex flex-wrap items-center gap-2 border-b px-3 py-2">
-		<li
-			class="
-			{activeClass(undefined)}
-			px-2 py-1 border rounded-xl hover:bg-primary-500 hover:text-secondary-500 transition-all"
+	<div class="border-b px-3 py-2">
+		<button
+			class="md:hidden btn justify-between w-56 px-2 py-1 rounded-xl border"
+			use:popup={projectTypePopup}
 		>
-			<a href="/projects" class="flex items-center gap-1">
-				<iconify-icon icon="ic:round-select-all" />
-				All
-			</a>
-		</li>
-		{#each projectTypes as projectType}
+			<span class="capitalize">{selectedType() || 'All'}</span>
+			<span>⏷</span>
+		</button>
+
+		<div
+			class="bg-secondary-500 shadow-lg py-2 border rounded-xl relative z-50"
+			data-popup="projectTypePopup"
+		>
+			<nav class="list-nav">
+				<ul class="p-1">
+					<li class="{activeClass(undefined)} rounded-xl">
+						<a href="/projects">All</a>
+					</li>
+					{#each projectTypes as projectType}
+						<li class="{activeClass(projectType.slug)} rounded-xl">
+							<a href="/projects/{projectType.slug}">
+								{projectType.title}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</nav>
+		</div>
+
+		<ul class="hidden md:flex flex-wrap items-center gap-2">
 			<li
 				class="
-				{activeClass(projectType.slug)} 
-				px-2 py-1 border rounded-xl hover:bg-primary-500 hover:text-secondary-500 transition-all
-			"
+				{activeClass(undefined)}
+				px-2 py-1 border rounded-xl hover:bg-primary-500 hover:text-secondary-500 transition-all"
 			>
-				<a href="/projects/{projectType.slug}" class="flex items-center gap-1 whitespace-nowrap">
-					<iconify-icon icon={projectType.icon} />
-					{projectType.title}
+				<a href="/projects" class="flex items-center gap-1">
+					<iconify-icon icon="ic:round-select-all" />
+					All
 				</a>
 			</li>
-		{/each}
-	</ul>
+			{#each projectTypes as projectType}
+				<li
+					class="
+					{activeClass(projectType.slug)}
+					px-2 py-1 border rounded-xl hover:bg-primary-500 hover:text-secondary-500 transition-all
+				"
+				>
+					<a href="/projects/{projectType.slug}" class="flex items-center gap-1 whitespace-nowrap">
+						<iconify-icon icon={projectType.icon} />
+						{projectType.title}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 {:else}
 	<div class="flex items-center border-b gap-2 px-3 py-2">
 		<a
